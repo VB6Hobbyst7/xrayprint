db551e3855a65a96b0e32b9693e31f838b5e3396
Chutchai S <Chutchai@lcb1.com>
2018-05-28 11:51:41 +0700

    Initial version

---
+++ XrayPrint/Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnQuery = New System.Windows.Forms.Button()
+        Me.txtBooking = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.rbB1 = New System.Windows.Forms.RadioButton()
+        Me.rbA0 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.rbCheck = New System.Windows.Forms.RadioButton()
+        Me.rbXray = New System.Windows.Forms.RadioButton()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.txtPlateNumber = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblLocation = New System.Windows.Forms.Label()
+        Me.lblContainer = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblRecord = New System.Windows.Forms.Label()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnQuery)
+        Me.GroupBox1.Controls.Add(Me.txtBooking)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 1)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(481, 68)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Booking Info"
+        '
+        'btnQuery
+        '
+        Me.btnQuery.Location = New System.Drawing.Point(266, 29)
+        Me.btnQuery.Name = "btnQuery"
+        Me.btnQuery.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuery.TabIndex = 2
+        Me.btnQuery.Text = "Get Detail"
+        Me.btnQuery.UseVisualStyleBackColor = True
+        '
+        'txtBooking
+        '
+        Me.txtBooking.Location = New System.Drawing.Point(59, 29)
+        Me.txtBooking.Name = "txtBooking"
+        Me.txtBooking.Size = New System.Drawing.Size(200, 20)
+        Me.txtBooking.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Booking"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 91)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(672, 399)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.GroupBox4)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Controls.Add(Me.btnPrint)
+        Me.GroupBox2.Controls.Add(Me.txtPlateNumber)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.lblLocation)
+        Me.GroupBox2.Controls.Add(Me.lblContainer)
+        Me.GroupBox2.Location = New System.Drawing.Point(693, 75)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(218, 415)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Fast Lane Print"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.rbB1)
+        Me.GroupBox4.Controls.Add(Me.rbA0)
+        Me.GroupBox4.Location = New System.Drawing.Point(113, 67)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(89, 69)
+        Me.GroupBox4.TabIndex = 14
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Terminal"
+        '
+        'rbB1
+        '
+        Me.rbB1.AutoSize = True
+        Me.rbB1.Location = New System.Drawing.Point(6, 42)
+        Me.rbB1.Name = "rbB1"
+        Me.rbB1.Size = New System.Drawing.Size(38, 17)
+        Me.rbB1.TabIndex = 8
+        Me.rbB1.Text = "B1"
+        Me.rbB1.UseVisualStyleBackColor = True
+        '
+        'rbA0
+        '
+        Me.rbA0.AutoSize = True
+        Me.rbA0.Checked = True
+        Me.rbA0.Location = New System.Drawing.Point(6, 19)
+        Me.rbA0.Name = "rbA0"
+        Me.rbA0.Size = New System.Drawing.Size(38, 17)
+        Me.rbA0.TabIndex = 7
+        Me.rbA0.TabStop = True
+        Me.rbA0.Text = "A0"
+        Me.rbA0.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.rbCheck)
+        Me.GroupBox3.Controls.Add(Me.rbXray)
+        Me.GroupBox3.Location = New System.Drawing.Point(10, 67)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(97, 69)
+        Me.GroupBox3.TabIndex = 13
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Type"
+        '
+        'rbCheck
+        '
+        Me.rbCheck.AutoSize = True
+        Me.rbCheck.Location = New System.Drawing.Point(8, 42)
+        Me.rbCheck.Name = "rbCheck"
+        Me.rbCheck.Size = New System.Drawing.Size(69, 17)
+        Me.rbCheck.TabIndex = 14
+        Me.rbCheck.Text = "เปิดตรวจ"
+        Me.rbCheck.UseVisualStyleBackColor = True
+        '
+        'rbXray
+        '
+        Me.rbXray.AutoSize = True
+        Me.rbXray.Checked = True
+        Me.rbXray.Location = New System.Drawing.Point(8, 19)
+        Me.rbXray.Name = "rbXray"
+        Me.rbXray.Size = New System.Drawing.Size(46, 17)
+        Me.rbXray.TabIndex = 13
+        Me.rbXray.TabStop = True
+        Me.rbXray.Text = "Xray"
+        Me.rbXray.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(9, 202)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(193, 35)
+        Me.btnPrint.TabIndex = 9
+        Me.btnPrint.Text = "Print Fast-lane"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'txtPlateNumber
+        '
+        Me.txtPlateNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPlateNumber.Location = New System.Drawing.Point(9, 162)
+        Me.txtPlateNumber.Name = "txtPlateNumber"
+        Me.txtPlateNumber.Size = New System.Drawing.Size(194, 29)
+        Me.txtPlateNumber.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Vehicle License no :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Position :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Container :"
+        '
+        'lblLocation
+        '
+        Me.lblLocation.AutoSize = True
+        Me.lblLocation.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLocation.Location = New System.Drawing.Point(70, 40)
+        Me.lblLocation.Name = "lblLocation"
+        Me.lblLocation.Size = New System.Drawing.Size(25, 24)
+        Me.lblLocation.TabIndex = 1
+        Me.lblLocation.Text = "..."
+        '
+        'lblContainer
+        '
+        Me.lblContainer.AutoSize = True
+        Me.lblContainer.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContainer.Location = New System.Drawing.Point(70, 16)
+        Me.lblContainer.Name = "lblContainer"
+        Me.lblContainer.Size = New System.Drawing.Size(25, 24)
+        Me.lblContainer.TabIndex = 0
+        Me.lblContainer.Text = "..."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 75)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Total :"
+        '
+        'lblRecord
+        '
+        Me.lblRecord.AutoSize = True
+        Me.lblRecord.Location = New System.Drawing.Point(55, 75)
+        Me.lblRecord.Name = "lblRecord"
+        Me.lblRecord.Size = New System.Drawing.Size(13, 13)
+        Me.lblRecord.TabIndex = 4
+        Me.lblRecord.Text = "0"
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(919, 502)
+        Me.Controls.Add(Me.lblRecord)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Xray Fast-Lane Request"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnQuery As Button
+    Friend WithEvents txtBooking As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblLocation As Label
+    Friend WithEvents lblContainer As Label
+    Friend WithEvents txtPlateNumber As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents rbB1 As RadioButton
+    Friend WithEvents rbA0 As RadioButton
+    Friend WithEvents rbCheck As RadioButton
+    Friend WithEvents rbXray As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblRecord As Label
+    Friend WithEvents PrintDialog1 As PrintDialog
+End Class
---
+++ XrayPrint/frmMain.vb
@@ -0,0 +1,223 @@
+﻿Imports System.Data.Odbc
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+'Imports Data.OleDb
+'Imports Microsoft.Data.Odbc
+
+Public Class Form1
+    Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
+        getContainer(txtBooking.Text.Trim)
+    End Sub
+
+    Sub getContainer(vBooking As String)
+        If vBooking = "" Then
+            MsgBox("Booking is required, please enter booking number")
+            Exit Sub
+        End If
+        clear()
+        Dim dtBooking As DataTable
+        dtBooking = accbynodes(vBooking)
+        Dim x As Integer = dtBooking.Rows.Count
+        lblRecord.Text = Str(x) + " Container(s)"
+        DataGridView1.DataSource = dtBooking
+    End Sub
+
+    Sub clear()
+        lblRecord.Text = ""
+        txtPlateNumber.Text = ""
+        lblContainer.Text = ""
+        lblLocation.Text = ""
+        rbA0.Checked = True
+        rbXray.Checked = True
+    End Sub
+
+    Friend Function accbynodes(ByVal booking As String) As DataTable
+
+        Dim cmd As New OdbcCommand
+        Dim sql As String
+        Dim da As New OdbcDataAdapter
+        'Readconnectionstring()
+        'Dim con As New OdbcConnection("Server='" & server & "';DATABASE='" & paraudstat.compid & "';Integrated Security='SSPI';")
+        'Dim con As New OdbcConnection("Provider=MSDASQL;driver={SQL Server};Intial Catalog='SAMLTD';Data Source='ACCPAC';User Id='sa';Password='cats@123';") 'Trusted_Connection=Yes;") 'Trusted_Connection=Yes;persist security info=False;")
+
+        'OdbcConnection
+        Dim con As OdbcConnection
+        Dim connectionString As String
+        connectionString = "DSN=CTCS1;UserID=OPSCC;Password=OPSCC21;DataCompression=True;"
+        con = New OdbcConnection(connectionString)
+        con.Open()
+
+
+        sql = "SELECT 
+                BOOKLIST.CNID94 as container,
+                BOOKLIST.HIDT94 as date_in, 
+                CTHNDL09.HDDT03 as date_out,
+                BOOKLIST.CNPT03 as location,
+                BOOKLIST.LYND03 as line,
+                BOOKLIST.ORGV05 as agent,
+                BOOKLIST.CNIS03 as iso,
+                BOOKLIST.CNLL03 as size,
+                BOOKLIST.ORRF93 as Booking,
+                BOOKLIST.VMID01 as vessel_code,
+                BOOKLIST.MVVA47 as vessel_name,
+                BOOKLIST.MVV247 as vessel_type,
+                BOOKLIST.RSIN01 as voy_in, 
+                BOOKLIST.RSUT01 as voy_out, 
+                BOOKLIST.OP0103 
+                FROM S2114C2V.LCB1DAT.BOOKLIST BOOKLIST
+                INNER JOIN CTHNDL09 ON BOOKLIST.HDID94 = CTHNDL09.HDID03
+                where BOOKLIST.ORRF93 = ? "
+
+
+        ' Create an OleDbDataAdapter object
+        Dim adapter As OdbcDataAdapter = New OdbcDataAdapter()
+        'adapter.SelectCommand = New OdbcCommand(sql, con)
+
+
+        Dim ds As New DataTable
+
+        ' Create Data Set object
+        'Dim ds As DataSet = New DataSet("orders")
+        ' Call DataAdapter's Fill method to fill data from the
+        ' DataAdapter to the DataSet 
+        'adapter.Fill(ds)
+
+        With cmd
+            .CommandText = sql
+            .CommandType = CommandType.Text
+            .Connection = con
+            .CommandTimeout = 100
+            .Parameters.Add(New OdbcParameter("booking", booking))
+
+        End With
+
+
+        da.SelectCommand = cmd
+        da.Fill(ds)
+
+        Return ds
+
+
+    End Function
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        'Dim selectedCellCount As String = DataGridView1.SelectedRows(0).Cells(0).ToString
+        Dim i As Integer
+        i = DataGridView1.CurrentRow.Index
+        lblContainer.Text = DataGridView1.Item(0, i).Value
+        lblLocation.Text = DataGridView1.Item(3, i).Value
+
+        Dim vAgent As String = Trim(DataGridView1.Item(5, i).Value)
+        rbA0.Checked = True
+        rbB1.Checked = IIf(vAgent = "MSK1" Or vAgent = "MSC", True, False)
+        'rbB1.Checked = IIf(vAgent = "MSC", True, False)
+
+        'MsgBox(selectedCellCount)
+    End Sub
+
+    Private Sub fill_to_excel(vContainer As String, vPosition As String,
+                              vTerminal As String, vPlate As String,
+                              vType As String)
+        Dim xlsWorkBook As Microsoft.Office.Interop.Excel.Workbook
+        Dim xlsWorkSheet As Microsoft.Office.Interop.Excel.Worksheet
+        Dim xls As New Microsoft.Office.Interop.Excel.Application
+
+        'Dim resourcesFolder = IO.Path.GetFullPath(Application.StartupPath & "\..\..\Resources\")
+        Dim resourcesFolder = IO.Path.GetFullPath(Application.StartupPath & "\")
+        Dim fileName = "FastlaneTemplate.xlsx"
+
+        xlsWorkBook = xls.Workbooks.Open(resourcesFolder & fileName)
+        xlsWorkSheet = xlsWorkBook.Sheets("Sheet2")
+
+        xlsWorkSheet.Cells(5, 2) = Now()
+        xlsWorkSheet.Cells(6, 2) = vType
+        xlsWorkSheet.Cells(7, 10) = vPlate
+        xlsWorkSheet.Cells(6, 10) = vTerminal
+        xlsWorkSheet.Cells(9, 10) = vPosition
+
+        xlsWorkSheet.Cells(9, 2) = vContainer
+
+
+        'xlsWorkBook.Close()
+        'xls.Quit()
+
+        xls.DisplayAlerts = False
+
+
+        xlsWorkBook.Close(True)
+
+
+
+        releaseObject(xlsWorkSheet)
+        releaseObject(xlsWorkBook)
+        releaseObject(xls)
+
+        'MsgBox("file saved to " & resourcesFolder)
+
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        If txtPlateNumber.Text = "" Then
+            MsgBox("Plate number is required, please enter!!")
+            txtPlateNumber.Focus()
+            Exit Sub
+        End If
+
+        fill_to_excel(lblContainer.Text.Trim,
+                      lblLocation.Text.Trim,
+                      IIf(rbA0.Checked, "A0", "B1"),
+                      txtPlateNumber.Text.Trim,
+                      IIf(rbXray.Checked, "X-RAY", "เปิดตรวจ"))
+
+        print_excel()
+        'If PrintDialog1.ShowDialog = DialogResult.OK Then
+        '    ' print the document
+        '    print_excel()
+        'End If
+
+    End Sub
+
+
+    Sub print_excel()
+        Dim strFile As String = "FastlaneTemplate.xlsx"
+        Dim objProcess As New System.Diagnostics.ProcessStartInfo
+
+        With objProcess
+            .FileName = strFile
+            .WindowStyle = ProcessWindowStyle.Hidden
+            .Verb = "print"
+
+
+            .CreateNoWindow = True
+            .UseShellExecute = True
+        End With
+        Try
+            System.Diagnostics.Process.Start(objProcess)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+
+    Private Sub txtBooking_TextChanged(sender As Object, e As EventArgs) Handles txtBooking.TextChanged
+
+    End Sub
+
+    Private Sub txtBooking_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtBooking.KeyPress
+        If e.KeyChar = Chr(13) Then
+            getContainer(txtBooking.Text.Trim)
+        End If
+
+    End Sub
+End Class
